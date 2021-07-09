900dd71a668feba85d633c58635bd288bc762cab
Shohanur Rahman <srshohan026@gmail.com>
2021-07-09 09:53:05 +0600

    initial setup

---
+++ script.js
@@ -0,0 +1,15 @@
+let searchBtn = document.querySelector('#searchBtn')
+let searchUser= document.querySelector('#searchUser')
+
+searchBtn.addEventListener('click', (e)=>{
+                           let userText= searchUser.value;
+                           if(userText!=' '){
+                               alert(userText)
+                           }
+                           console.log(userText)
+                        
+})
+let sdf= document.getElementById({
+    
+})
+
